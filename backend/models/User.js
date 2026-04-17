c133fde85b71d90ee63327ce4a7592a75fce6f8f
Glaiza Mangaoang <gmmangaoang23109073@student.dmmmsu.edu.ph>
2026-04-17 12:52:17 +0800

    Add password hashing pre-save hook

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,6 +12,20 @@ const userSchema = new mongoose.Schema({
   profilePic: { type: String, default: '' },
 }, { timestamps: true });
 
+// ✅ PRE-SAVE HOOK: Hash password before saving
+userSchema.pre('save', async function(next) {
+  // Only hash if password is modified (or new)
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Method to check password
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
